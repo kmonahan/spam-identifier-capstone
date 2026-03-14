dd590dc2f3792eba200e675176538604c98ee617
KJ Monahan <kejm42@gmail.com>
2026-03-14 13:04:21 -0500

    Start of report; fixing a bunch of stuff

--- spam-identification.R
+++ spam-identification.R
@@ -25,33 +25,71 @@ library(tidytext)
 if (!require(tokenizers))
   install.packages('tokenizers')
 library(tokenizers)
+if (!require(textutils))
+  install.packages('textutils')
+library(textutils)
+if (!require(caret))
+  install.packages('caret')
+library(caret)
+if (!require(ggridges))
+  install.packages('ggridges')
+library(ggridges)
 
-# Load and clean
-messages <- read_tsv(file = "data/SMSSpamCollection", col_names = FALSE)
+#############################################################################
+# Load and clean data
+#############################################################################
+
+# Using read_tsv() resulted in 4773 rows.
+# messages <- read_tsv(file = "data/SMSSpamCollection", col_names = FALSE, col_types = c('c', 'c'))
+# colnames(messages) <- c("type", "message")
+# messages <- messages |> mutate(type = factor(type, levels = c('spam', 'ham')))
+
+# Reading the lines and splitting the strings without using read_tsv gave the
+# correct number of rows.
+lines <- readLines("data/SMSSpamCollection")
+rows <- str_split(lines, "\t")
+messages <- data.frame(parse_factor(map_chr(rows, 1)), parse_character(map_chr(rows, 2)))
 colnames(messages) <- c("type", "message")
-messages <- messages |> mutate(type = factor(type, levels = c('spam', 'ham')))
-
-# Randomly choose 20% of the data set to set aside as our test set.
-# Set a seed so random selection stays the same between runs.
-
-# TODO: Why 80%/20%??
-set.seed(2016)
-test_index <- createDataPartition(messages$type,
-                                  times = 1,
-                                  p = 0.2,
-                                  list = FALSE)
-test_set <- messages[test_index, ]
-train_set <- messages[-test_index, ]
 
 # Data exploration
 # Look at first 10 items
-head(train_set, 10) |> tibble()
+head(messages, 10) |> tibble()
 
+# Look at the most popular words
+messages |>
+  unnest_tokens(word, message, to_lower=TRUE, strip_punct=TRUE) |>
+  # Filter out the stop words because they're not interesting
+  filter(!word %in% stop_words$word) |>
+  count(word) |>
+  top_n(10, n) |>
+  mutate(word = reorder(word, n)) |>
+  arrange(desc(n))
 
+# Where are those lts and gts coming from? Look at some of the messages that
+# contain them. tokenize_words() is what unnest_tokens uses to split the words
+# into tokens. Using that here in order to use the tokens as part of the filtering
+# rather than as a separate data frame.
+messages |>
+  filter(sapply(message, function(m) {
+    words <- list_c(tokenize_words(m, lowercase = TRUE, strip_punct=TRUE))
+    any(c('lt', 'gt') %in% words)
+  })) |>
+  pull(message) |>
+  head()
 
-# Notice that in this sample, spam messages contain phone numbers but ham
-# messages do not. Is that often the case?
-# First, standardize phone numbers.
+# Decode HTML entities and remove any HTML tags that might still be lingering.
+messages <- messages |> mutate(message=HTMLrm(HTMLdecode(message)))
+# Check top words (without stop words) again. That looks better.
+messages |>
+  unnest_tokens(word, message, to_lower=TRUE, strip_punct=TRUE) |>
+  filter(!word %in% stop_words$word) |>
+  count(word) |>
+  top_n(10, n) |>
+  mutate(word = reorder(word, n)) |>
+  arrange(desc(n))
+
+# Next, standardize phone numbers, so they're a bit easier to identify, and so
+# we can look for long strings of numbers in general.
 convert_phone_numbers <- function(s) {
   s |>
     # Handle formats 000-000-0000, 000.000.000, and 000 000 0000
@@ -61,7 +99,7 @@ convert_phone_numbers <- function(s) {
     str_replace_all("\\((\\d{3})\\)\\s?(\\d{3})-(\\d{4})", "\\1\\2\\3")
 }
 
-# Test that our conversion function is working
+# Test that the conversion function is working
 phone_test_data <- c(
   'Phone number 0123456789',
   'Phone number 012-345-6789',
@@ -75,36 +113,33 @@ all(sapply(convert_phone_numbers(phone_test_data), function(n) {
   identical(n, phone_test_data[1])
 }))
 
-train_set <- train_set |>
+# Success! So now standardize all the phone numbers in the messages.
+messages <- messages |>
   mutate(message = convert_phone_numbers(message))
-test_set <- test_set |>
-  mutate(message = convert_phone_numbers(message))
 
-# Look for numbers 5-11 digits: 5 is the text shorthand (e.g. text to 87121)
-# and 11 is a full phone number with country code, (08002986030).
-messages_with_numbers <- train_set |> filter(str_detect(message, "\\d{5,11}"))
-messages_with_numbers |> ggplot(aes(x = type, y = after_stat(count), fill =
-                                      type)) + geom_bar()
-
-# Yup, overwhelmingly messages with long numbers are spam.
+##############################################################################
+# Analysis and variable calculation
+##############################################################################
 
 
 
-# Try by most popular words
-# Pick an example first to see how does.
-# It's 3/10, so we'll use 310
-train_set[310, ] |>
-  unnest_tokens(word, message) |>
-  select(word)
+# Randomly choose 20% of the data set to set aside as our test set.
+# Set a seed so random selection stays the same between runs.
+set.seed(2016)
+test_index <- createDataPartition(messages$type,
+                                  times = 1,
+                                  p = 0.2,
+                                  list = FALSE)
+test_set <- messages[test_index, ]
+train_set <- messages[-test_index, ]
 
-# Looks good, so let's try all the words.
-# We should filter out stop words too to avoid just getting a bunch of prepositions.
+
+# Look at the most commonly used words in the training set, by message type.
+# Are there differences?
 train_set_words <- train_set |>
-  unnest_tokens(word, message) |>
+  unnest_tokens(word, message, to_lower=TRUE, strip_punct=TRUE) |>
   filter(!word %in% stop_words$word)
-head(train_set_words)
 
-# Display the most commonly used words for ham versus spam
 train_set_words |>
   filter(type == 'ham') |>
   count(word) |>
@@ -119,229 +154,195 @@ train_set_words |>
   mutate(word = reorder(word, n)) |>
   arrange(desc(n))
 
-# There's a lot of overlap, but we can see some differences. "free" appears more
-# frequently in spam messages, as does "txt". "gt" and "lt" appear in ham more
-# often. Interestingly, "ham" also shows up a lot in ham messages, which makes
-# me wonder if the classification is bleeding into the message. Let's check.
-
-# Add row ID so we can use it to identify messages
-train_set <- train_set |>
-  rowid_to_column()
-
-# Re-create the words so we can use our ID
-train_set_words <- train_set |>
-  unnest_tokens(word, message) |>
-  filter(!word %in% stop_words$word)
-
-# Get the IDs of messages with "ham"
-ham_rows <- train_set_words |>
-  filter(word == "ham") |>
-  select(rowid) |>
-  unique()
-ham_rows
-
-# This one looks fine.
-train_set[1856, ]
-# THis one, on the other hand, looks suspicious.
-train_set[3650, ]
-
-# Now we can see that it's mostly from the very long message at row 3650, which
-# uses "\nham\t" and "\nspam\t". It looks like this message is actually multiple
-# messages, some of which are ham and some of which are spam.
-
-# So we need to clean it and any other messages like it up, which will change
-# our spam and ham contents, so we should re-run the models after doing so.
-
-# Find the relevant messages
-# TODO: Make this a function
-dirty_data <- train_set |>
-  filter(str_detect(message, "\n(spam|ham)\t"))
-# Take the row out of train_set
-# TODO: Use some better set function here?
-train_set <- train_set |>
-  filter(!rowid %in% dirty_data$rowid)
-# Split the data by the newline column
-lines <- str_split_1(dirty_data$message, '\n')
-clean_data <- str_split(lines, "\t")
-first_row <- clean_data[[1]]
-# If the first row doesn't contain ham/spam, then its value was already parsed
-# as the type for the original row.
-if (!str_detect(first_row, "^(ham|spam)")) {
-  # Add the first row to the data set.
-  train_set <- train_set |> add_row(type = dirty_data$type, message = parse_guess(map_chr(first_row, 1)))
-  # Remove it from the processing we're going to do next
-  clean_data <- clean_data[-1]
-}
-clean_data <- clean_data |>
-  transpose() |>
-  map(~ parse_guess(unlist(.))) |>
-  setNames(colnames(train_set)[-1]) |>
-  as.data.frame() |>
-  mutate(type = factor(type, levels = levels(train_set$type)))
-# Add our cleaned up data back to the train_set
-# Remove the ID column we added for identification
-train_set <- train_set[, -1]
-train_set <- rbind(train_set, clean_data)
-train_set <- train_set |>
-  mutate(message = convert_phone_numbers(message))
-
-# Now that that's straightened out, let's look at the top words again
-train_set_words <- train_set |>
-  unnest_tokens(word, message) |>
-  filter(!word %in% stop_words$word)
-head(train_set_words)
-
-# Display the most commonly used words for ham versus spam
-train_set_words |>
-  filter(type == 'ham') |>
-  count(word) |>
-  top_n(10, n) |>
-  mutate(word = reorder(word, n)) |>
-  arrange(desc(n))
-
-train_set_words |>
-  filter(type == 'spam') |>
-  count(word) |>
-  top_n(10, n) |>
-  mutate(word = reorder(word, n)) |>
-  arrange(desc(n))
-
-# Can still observe some differences. "gt" and "lt" appear more often in ham
-# messages. "txt", "free," "claim," and "mobile" are more common in spam messages.
-# Some, like "call" are common in both.
-
+# Calculate the odds of a word being ham versus spam, so we can identify
+# which words are more likely to be in spam messages, versus words that are
+# likely to be any type of message.
 # Compute the odds of each word being in ham versus spam
 ham_spam_odds <- train_set_words |>
-  # Filter out numbers, we already looked at those
-  filter(!str_detect(word, "\\d+")) |>
   # Counts the number of items the word appears in each type of message
   count(word, type) |>
   # Splits type into 2 columns
   spread(type, n, fill = 0) |>
-  # Compute odds ratio
-  mutate(or = (spam + 0.5) / (sum(spam) - spam + 0.5) /
+  # Compute odds ratio, using the 0.5 correction
+  # This follows https://rafalab.dfci.harvard.edu/dsbook/text-mining.html
+  mutate(odds_ratio = (spam + 0.5) / (sum(spam) - spam + 0.5) /
            ((ham + 0.5) / (sum(ham) - ham + 0.5))) |>
   # Order by odds
-  top_n(20, or) |>
-  arrange(desc(or))
-ham_spam_odds
+  arrange(desc(odds_ratio))
+ham_spam_odds |>
+  head(15) |>
+  tibble()
 
-# TODO: Graph these somehow?
+# How many of the 7513 words have an odds ratio greater than 1?
+nrow(ham_spam_odds |> filter(odds_ratio > 1))
 
-# Now we can see that there are words that are far more likely to be in spam
-# messages than ham messages.
-top_20_spam_words <- ham_spam_odds$word
+# What about greater than 10?
+nrow(ham_spam_odds |> filter(odds_ratio > 10))
 
-# length of messages
-# Can see that on average, spam messages are slightly longer than ham messages
-train_set |>
-  group_by(type) |>
-  summarize(avg_length = mean(nchar(message)))
+# Get the words with an odds ratio of 10 or more. That's the list of words
+# that are at least 10x more likely to be in a spam message than a ham message.
+top_spam_words <- ham_spam_odds |> filter(odds_ratio >= 10) |> pull('word')
 
-# Looks like there might be both a min and a max for spam
+# Count how many times any of those words appear in the message
+count_spam_words <- function(data, word_list = top_spam_words) {
+  data |>
+    mutate(top_50_word_count = sapply(message, function(m) {
+      words <- list_c(tokenize_words(m, lowercase = TRUE, strip_punct=TRUE))
+      sum(words %in% top_50_spam_words)
+    }))
+}
+
+train_set <- count_spam_words(train_set)
+test_set <- count_spam_words(test_set)
+
+# Now let's see how many numbers in total spam versus ham messages have and
+# how long the longest string of numbers is (now that phone numbers will show
+# up as one unbroken string). Are there any differences?
+add_number_columns <- function(data) {
+  data |>
+    mutate(message = convert_phone_numbers(message)) |>
+    mutate(num_count = str_count(message, "\\d")) |>
+    mutate(longest_number = unlist(lapply(str_match_all(message, "\\d+"), function(m) {
+      if (length(m) == 0) {
+        0
+      } else {
+        max(nchar(m[,1]))
+      }
+    })))
+}
+train_set <- add_number_columns(train_set)
+test_set <- add_number_columns(test_set)
+
+# Box plot comparing total number of digits
 train_set |> 
-  mutate(msg_length = nchar(message)) |>
-  ggplot(aes(msg_length, fill=type)) + 
-  geom_histogram(binwidth=10) +
-  facet_grid(type ~ .)
+  ggplot(aes(type, num_count, fill=type)) +
+  xlab("Message type") +
+  ylab("Number of digits in message") +
+  ggtitle("Comparison of digits per message by message type") +
+  geom_boxplot()
 
-# Total capital letters
-train_set |>
-  mutate(capital_letters = str_count(message, "[A-Z]")) |>
-  group_by(type) |>
-  summarize(avg_capitals = mean(capital_letters), sd = sd(capital_letters))
+# Box plot to compare longest string of numbers by type of message
+train_set |> 
+  ggplot(aes(type, longest_number, fill=type)) + 
+  xlab("Message type") +
+  ylab("Longest digits in message") +
+  ggtitle("Comparison of lengths of longest string of digits per message by message type") +
+  geom_boxplot()
+
+
+
+# Count the total number of capital letters and the longest string of at least two
+# characters that starts with a capital and doesnot contain a lowercase letter 
+# or digit. "URGENT!! CALL ME!" should count as 17, while "Urgent!! call ME!" 
+# should count as 3. (Both counts include punctuation if it's within the string.)
+add_capital_columns <- function(data) {
+  data |>
+    mutate(capital_count = str_count(message, "[A-Z]")) |>
+    mutate(longest_capitals = unlist(lapply(str_match_all(message, "[A-Z][^a-z\\d]*[A-Z]"), function(m) {
+      if (length(m) == 0) {
+        0
+      } else {
+        max(nchar(m[,1]))
+      }
+    })))
+}
+
+train_set <- add_capital_columns(train_set)
+test_set <- add_capital_columns(test_set)
+
+# Box plot to compare total number of capital letters by type
+train_set |> 
+  ggplot(aes(type, capital_count, fill=type)) + 
+  xlab("Message type") +
+  ylab("Number of capital letters (A-Z)") +
+  ggtitle("Comparison of number of capital letters per message by message type") +
+  geom_boxplot()
+
+# Ridge plot to compare the total number of capital letters by type
+train_set |> 
+  ggplot(aes(capital_count, type, fill=type)) + 
+  ylab("Message type") +
+  xlab("Number of capital letters (A-Z)") +
+  ggtitle("Comparison of number of capital letters per message by message type") +
+  geom_density_ridges(bandwidth = 1, alpha=0.5)
+
+# Box plot to compare longest string
+train_set |> 
+  ggplot(aes(type, longest_capitals, fill=type)) + 
+  xlab("Message type") +
+  ylab("Longest string of capital letters") +
+  ggtitle("Comparison of lenght of the longest string of capital letters per message by message type") +
+  geom_boxplot()
 
 train_set |> 
-  mutate(capital_letters = str_count(message, "[A-Z]")) |>
-  ggplot(aes(capital_letters, fill=type)) + 
-  geom_histogram(bins=25) +
-  facet_grid(type ~ .)
+  ggplot(aes(longest_capitals, type, fill=type)) + 
+  ylab("Message type") +
+  ylab("Longest string of capital letters") +
+  ggtitle("Comparison of lenght of the longest string of capital letters per message by message type") +
+  geom_density_ridges(bandwidth = 1.25, alpha=0.5)
 
-# Can see that messages with a lot of capital letters tend to be spam, but there's
-# overlap.
+# Finally, what about the total length of the message?
+train_set <- train_set |> mutate(msg_length = nchar(message))
+test_set <- test_set |> mutate(msg_length = nchar(message))
 
-# Longest string of capital letters
-train_set <- train_set |>
-  mutate(longest_capitals = unlist(lapply(str_match_all(message, "[A-Z]+"), function(m) {
-    if (length(m) == 0) {
-      0
-    } else {
-      max(nchar(m[,1]))
-    }
-  }))
-
-train_set |>
-  group_by(type) |>
-  summarize(mean = mean(longest_capitals), sd = sd(longest_capitals))
-
-# Can see that spam messages are more likely to use longer strings of capitals
-train_set |> 
-  ggplot(aes(longest_capitals, fill=type)) + 
-  geom_histogram(binwidth=1) +
-  facet_grid(type ~ .)
+# We can see a difference here although, as with capital letters, it's not
+# as pronounced as some of the others. But spam messages do seem to be more
+# often longer than ham messages up to a point. Messages above 250 characters
+# are uncommon, but they're ham when they do appear.
+train_set |> ggplot(aes(type, msg_length, fill=type)) + geom_boxplot()
 
 # Model 1: Baseline
 # Just guess, assuming equal likelihood messages are spam or ham
 guess_y_hat <- sample(c('spam', 'ham'), nrow(test_set), replace = TRUE) |>
   factor(levels = levels(test_set$type))
-# Use F_meas to calculate the F1 score. Use beta of 0.5 because it's more
+# Use F_meas to calculate the F1 score. Use beta of 0.75 because it's more
 # important that a user not miss a real message.
-F_meas(
+guess_f_meas <- F_meas(
   guess_y_hat,
   reference = test_set$type,
   relevant = "spam",
   beta = 0.75
 )
+guess_f_meas
 
-# Model 2: Based on numbers
-has_number_y_hat <- ifelse(str_detect(test_set$message, "\\d{5,11}"), "spam", "ham") |>
+# Model 2: Based on the longest number. Of the various factors,
+# a long number had the most dramatic difference. If a number has more than 5
+# digits, which is also the length of those text short numbers, assume it's spam.
+long_number_y_hat <- ifelse(test_set$longest_number >= 5, "spam", "ham") |>
   factor(levels = levels(test_set$type))
-F_meas(
-  has_number_y_hat,
+long_number_f_meas <- F_meas(
+  long_number_y_hat,
   reference = test_set$type,
   relevant = "spam",
   beta = 0.75
 )
+long_number_f_meas
 
-# Model 3: Based on words
-has_spam_words_y_hat <- sapply(test_set$message, function(m) {
-  words <- unlist(tokenize_words(m))
-  if (any(words %in% top_20_spam_words)) {
-    "spam"
-  } else {
-    "ham"
-  }
-}) |>
+# Model 3: Using linear regression with all factors
+# TODO: Isn't there a way to do all but one column instead?
+# TODO: Why linear regression?
+lm_all_fit <- mutate(train_set, y = as.numeric(type == "spam")) |> 
+  lm(y ~ top_50_word_count + num_count + longest_number + capital_count + longest_capitals + msg_length, data = _)
+lm_all_p_hat <- predict(lm_all_fit, test_set)
+lm_all_y_hat <- ifelse(lm_p_hat > 0.5, "spam", "ham") |> 
   factor(levels = levels(test_set$type))
-F_meas(
-  has_spam_words_y_hat,
+lm_all_f_meas <- F_meas(
+  lm_all_y_hat,
   reference = test_set$type,
   relevant = "spam",
   beta = 0.75
 )
+lm_all_f_meas
 
-# Model 4: Based on message length
-msg_length_y_hat <- ifelse(nchar(test_set$message) < 110 | nchar(test_set$message) > 150, "ham", "spam") |>
-  factor(levels = levels(test_set$type))
-F_meas(
-  msg_length_y_hat,
-  reference = test_set$type,
-  relevant = "spam",
-  beta = 0.75
-)
+# TODO: Is there a point to having both model 3 and model 4?
 
-# Model 5: Based on total capital letters, using roughly the average + 1 SD
-total_capitals_y_hat <- ifelse(str_count(test_set$message, "[A-Z]") > 15, "spam", "ham") |>
-  factor(levels = levels(test_set$type))
-F_meas(
-  total_capitals_y_hat,
-  reference = test_set$type,
-  relevant = "spam",
-  beta = 0.75
-)  
 
-# Looking at spambase, try capital letters too and length of message
+# glm (and also review how that's different from lm)
+# Loess (and also review loess)
+# K-nearest-neighbors
 # Classifciation tree
 # Random forest
-# other models?
 # Ensemble?
+
+# Is the answer going to end up being just checking if there's a number that's
+# 5 digits long?
